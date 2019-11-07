01e98553376e12e1ad97c8d23b907a94d7ca1c07
João Batista Pereira Matos Júnior <matosjunior.jb@gmail.com>
2019-11-07 14:39:25 -0400

    Update tripplet_data.py

--- tripplet_data.py
+++ tripplet_data.py
@@ -1,54 +1,75 @@
-import tensorflow as tf
-import random
+import tensorflow as tf; print(tf.__version__)
 import numpy as np
-import matplotlib.pyplot as plt
+import os
+import random
 
+def load_img(image_file):
+    return tf.io.read_file(image_file)
 
-def filter_possible(pid1, pids2, same=True):
-    person_number = pid1[0]
-    if same is True:
-        possible_ids2 = [pid2 for pid2 in pids2 if pid2[0] == person_number]
-    else:
-        possible_ids2 = [pid2 for pid2 in pids2 if pid2[0] != person_number]
-    return possible_ids2
+def img_to_array(image_raw):
+    return tf.image.decode_image(image_raw).numpy()
 
-def combine_cam_files(pids1, pids2):
-    i = 0
-    data = {}
-    def negative(i, pid1): 
-        if i % 2 == 0:
-            return (0, *random.choice(filter_possible(pid1, pids1, same=False)))
-        else:
-            return (1,  *random.choice(filter_possible(pid1, pids2, same=False)))
+def list_cam_files(cam_files_path):
+    return [filename for filename in os.listdir(cam_files_path) if '.' not in filename]
+
+def list_person_files(person_files_path):
+    return [filename for filename in os.listdir(person_files_path) if '.png' in filename and '_' not in filename]
+
+def map_person_files(data_path):
+    cam = list_cam_files(data_path)
+    return {person: [data_path + person + '/'] for person in cam}
+
+def data_generator(data_folder, dataset_size=1000):
+    data_path_cam0 = data_folder + '0/'
+    data_path_cam1 = data_folder + '1/'
+    cam = map_person_files(data_path_cam0)
+    cam1 = map_person_files(data_path_cam1)
+    
+    persons = [person for person in cam.keys()]
+    
+    for person in persons:
+        cam[person].extend(cam1[person])
         
-    for i, pid1 in enumerate(pids1):
-        data[(0, *pid1)] = { 
-            'pos': (1, *random.choice(filter_possible(pid1, pids2, same=True))),
-            'neg': negative(i, pid1) }
-    return data
-
-def pairs_labels(d_combination, batch):
-    same = lambda k1, k2: 1. if k1==k2 else 0.
-    for _ in range(batch):
-        for key, values in d_combination.items():
-            neg_value = random.choice(values['neg'])
-            yield(key, values['pos'], values['neg'])
-            
-def generate_data(cam_img_dict, pids1, pids2, batch=1):
+    def get_negative(person, persons):
+        while True:
+            _person = random.choice(persons)
+            if _person != person:
+                return _person
     
-    X_anchor = [] 
-    X_positive = []
-    X_negative = []
-    
-    for _ in range(batch):
-        combination_data = combine_cam_files(pids1, pids2)
-        data = list(pairs_labels(combination_data, batch))
-        X_anchor.extend([cam_img_dict[x[0]] for x in data])
-        X_positive.extend([cam_img_dict[x[1]] for x in data])
-        X_negative.extend([cam_img_dict[x[2]] for x in data])
+    def choose_file(data_path, ref_file=None):
+        while True:
+            img = data_path + random.choice(list_person_files(data_path))
+            if img != ref_file:
+                return img
         
-    X_anchor = np.array(X_anchor, dtype = np.float32)
-    X_positive = np.array(X_positive, dtype = np.float32)
-    X_negative = np.array(X_negative, dtype = np.float32)
-    
-    return X_anchor, X_positive, X_negative 
+    cams = [0, 1]
+    X_data = []
+    Y_data = []
+    for _ in range(dataset_size):
+        person = random.choice(persons)
+        nperson = get_negative(person, persons)
+        
+        anchor_person = random.choice(cams)
+        pos_person = random.choice(cams)
+        neg_person = random.choice(cams)
+        
+        anchor_img = choose_file(cam[person][anchor_person])
+        pos_img = choose_file(cam[person][pos_person], anchor_img)
+        neg_img = choose_file(cam[nperson][neg_person])
+        
+        anchor_img = load_img(cam[person][anchor_person])
+        pos_img = load_img(cam[person][pos_person], anchor_img)
+        neg_img = load_img(cam[nperson][neg_person])
+        
+        anchor_img = img_to_array(cam[person][anchor_person])
+        pos_img = img_to_array(cam[person][pos_person], anchor_img)
+        neg_img = img_to_array(cam[nperson][neg_person])
+        
+        X_data.append([anchor_img, pos_img, neg_img])
+        Y_data.append([person, nperson])
+    return np.array(X_data), np.array(Y_data)
+
+
+# train_data_path = './datasets/airport-alunos/treino/'
+# X_data, y_data = data_generator(train_data_path, dataset_size=2)
+# X_data
